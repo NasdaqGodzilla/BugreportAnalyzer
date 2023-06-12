f5cf2cf5a57989ea0f067651da60d2658c4bb79d
NasdaqGodzilla <26323326+NasdaqGodzilla@users.noreply.github.com>
2023-06-12 15:25:19 +0800

    [feature][battery_analyzer] Add job stats into battery_analyzer reporter via dump of job in battery history

--- battery/battery_analyzer.sh
+++ battery/battery_analyzer.sh
@@ -113,6 +113,46 @@ ExampleOutput
     done
 }
 
+# Parse battery history and make job summary
+# Output: Total job in battery history
+function battery_analyze_batteryhistory_job_summary() {
+    local dumpfile="$1"
+    local batteryhistory_dump=`batterystats_parse_get_batteryhistory_dump "$dumpfile"`
+    local batteryhistory_dump_job=`echo -e "$batteryhistory_dump" | \
+        awk '/job=/{printf $1" "; for(i=5; i<=NF; ++i)printf $i;printf "\n"}'`
+    unset batteryhistory_dump
+    local batteryhistory_dump_job_formatted=`echo -e "$batteryhistory_dump_job" | \
+        sed -e 's/^+//g; s/ [+-]/ & /g; s/:/ /g; s/=/ /g' | xargs printf "  %-20s%-4s%-28s%-16s%s\n"`
+    unset batteryhistory_dump_job
+
+    # 1h12m44s305ms       +   job                         1000            android/com.android.server.pm.DynamicCodeLoggingService
+    # echo -e "$batteryhistory_dump_job_formatted"
+
+    local all_names=`echo -e "$batteryhistory_dump_job_formatted" | \
+        awk '{$1="";$2="";$3="";$4="";print $0}' | \
+            sed 's/^[ ]*//g; s/[ ]*$//g' | \
+                sort | uniq`
+    # echo -e "$all_names"
+
+    echo -e "$all_names" | while read name; do
+        local name_no_wildcard=`echo -e "$name" | sed 's/\*/\\\*/g'`
+        local records=`echo -e "$batteryhistory_dump_job_formatted" | grep -w "$name_no_wildcard"`
+
+: << ExampleOutput
+android/com.android.server.net.watchlist.ReportWatchlistJobService:
+  1h12m44s319ms       +   job                         1000            android/com.android.server.net.watchlist.ReportWatchlistJobService
+  1h12m44s331ms       -   job                         1000            android/com.android.server.net.watchlist.ReportWatchlistJobService
+android/com.android.server.pm.DynamicCodeLoggingService:
+  1h12m44s305ms       +   job                         1000            android/com.android.server.pm.DynamicCodeLoggingService
+  1h12m44s528ms       -   job                         1000            android/com.android.server.pm.DynamicCodeLoggingService
+ExampleOutput
+        # echo -e "$name:" && echo -e "$records"
+
+        local total_job_active=`battery_analyze_batteryhistory_summary_activetime "$records"`
+        echo -e "$total_job_active"
+    done
+}
+
 function battery_analyze_summary() {
     local dumpfile="$1"
 
@@ -155,6 +195,16 @@ function battery_analyze_summary() {
         awk '{print $(NF-1)" "$0}' | sort -rgb | cut -f3- -d' '`
     echo -e "Name Seconds Time Counts" | xargs printf "\t%-88s%-12s%-12s%s\n"
     echo -e "$battery_alarm_history_sorted" | awk '{print "\t", $0}'
+    printf "]\n"
+
+    local batteryhistory_dump_job_summary=`battery_analyze_batteryhistory_job_summary "$dumpfile"`
+
+    printf "[  Battery Job History:\n"
+    local battery_job_history=`echo -e "$batteryhistory_dump_job_summary" | \
+        awk '{count=$NF;time=$(NF-2);time_formatted=$(NF-1);$NF="";$(NF-1)="";$(NF-2)="";printf "%-96s%-12s%-12s%s\n", $0, time, time_formatted, count}'`
+    local battery_job_history_sortted=`echo -e "$battery_job_history"`
+    echo -e "Name Seconds Time Counts" | xargs printf "\t%-88s%-12s%-12s%s\n"
+    echo -e "$battery_job_history_sortted" | awk '{print "\t", $0}'
     printf "]\n"
 
     printf "+ %.0s" {1..50} ; echo
